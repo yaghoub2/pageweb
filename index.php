76ebd70ec601c443293abd73f7ae120598cc3361
yaghoub <mail@quelbazar.com>
2019-01-19 14:53:17 +0100

    anna avec un index

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Canvas text animation example</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    
+    <!--[if IE]><script type="text/javascript" src="../excanvas.js"></script><![endif]-->
+    <script type="text/javascript" src="../canvas.text.js"></script>
+    <script type="text/javascript" src="../faces/optimer-normal-normal.js"></script>
+	</head>
+	<body>
+<div><h1>je suis dans la branche martial</h1></div>
+	</body>
+</html>
+
